a3eb693da16c4d73bfd6257f9df016c7cbe3f4d8
stonelv <zhe.lv.82@gmail.com>
2023-02-18 21:07:26 +0800

    init commit

---
+++ Controllers/OpenAIController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace openai_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OpenAIController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Ask([FromQuery] string question)
+        {
+            var api = new OpenAI_API.OpenAIAPI("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");
+            var result = await api.Completions.GetCompletion(question);
+            return Ok(result);
+        }
+    }
+}
