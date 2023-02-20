ebd0c956246081df0d18754456c92c16fb811e4c
lvzhe <zlv@mycwt.com>
2023-02-20 18:12:38 +0800

    change api key

--- Controllers/OpenAIController.cs
+++ Controllers/OpenAIController.cs
@@ -9,21 +9,32 @@ namespace openai_api.Controllers
     public class OpenAIController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private readonly string _apiKey;
 
         public OpenAIController(IConfiguration configuration)
         {
             _configuration = configuration;
+            _apiKey = _configuration.GetSection("OpenAIKey").Value;
         }
 
         [HttpGet]
         public async Task<IActionResult> Ask([FromQuery] string question)
         {
-            var apiKey = _configuration.GetSection("OpenAIKey").Value;
-            var api = new OpenAI_API.OpenAIAPI(apiKey);
+            var api = new OpenAI_API.OpenAIAPI(_apiKey);
             var request = new CompletionRequest(question, model: Model.DavinciText, 
                 temperature: 0.6, max_tokens: 500);
             var result = await api.Completions.CreateCompletionAsync(request);
             return Ok(result.Completions.FirstOrDefault()?.Text);
         }
+
+        //[Route("Image")]
+        //[HttpGet]
+        //public async Task<IActionResult> GetImage([FromQuery] string question)
+        //{
+        //    var api = new OpenAI_API.OpenAIAPI(_apiKey);
+        //    var request = new CompletionRequest(question, model: Model.DavinciText,
+        //        temperature: 0.6, max_tokens: 500);
+        //    var result = await api.Completions.
+        //}
     }
 }
